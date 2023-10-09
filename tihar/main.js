47769ff2853764deda14ae489b8695ce025acbca
anilghale1 <ghalepasang5@gmail.com>
2023-10-09 19:57:11 +0545

    updated circular motion in movement of circle

--- tihar/main.js
+++ tihar/main.js
@@ -3,53 +3,57 @@ const c = canvas.getContext("2d");
 const centerX = canvas.width / 2;
 const centerY = canvas.height / 2;
 const radius = 100; // Adjust the radius as needed
+const angularSpeed = 0.01; // Adjust the angular speed as needed
+const maxDistance = 5; // Maximum distance from the center
 
 class Tihar {
     constructor(angle) {
         this.angle = angle;
         this.x = centerX + radius * Math.cos(angle);
         this.y = centerY + radius * Math.sin(angle);
-        setInterval(() => {
-            // Call getRandomColor function here
+        this.distance = 0; // Initial distance from the center
+        this.opacity = 1; // Initial opacity
+        this.rotation = 0; // Initial rotation angle
+        setInterval(() => {           
             this.color = this.getRandomColor();
-            // Do something with the color, for example, log it
-            console.log(this.color);
         }, 200);
     }
 
     draw() {
-        
         c.beginPath();
         c.arc(this.x, this.y, 10, 0, 2 * Math.PI);
-        c.fillStyle = this.color;
+        c.fillStyle = `rgba(${this.color}, ${this.opacity})`;
         c.fill();
     }
 
     move() {
-        
+        if (this.distance < maxDistance) {
+            this.distance += 0.01; // Increase the distance from the center with a smaller increment
+        }
+        const expandFactor = Math.min(1, this.distance / 5); // Calculate the expand factor based on the distance
+        this.x = centerX + (radius + this.distance) * Math.cos(this.angle) * expandFactor;
+        this.y = centerY + (radius + this.distance) * Math.sin(this.angle) * expandFactor;
+        this.rotation += angularSpeed; // Update the rotation angle
+        const newX = (this.x - centerX) * Math.cos(this.rotation) - (this.y - centerY) * Math.sin(this.rotation);
+        const newY = (this.x - centerX) * Math.sin(this.rotation) + (this.y - centerY) * Math.cos(this.rotation);
+        this.x = newX + centerX;
+        this.y = newY + centerY;
     }
 
     update() {
-        this.draw();
         this.move();
-        
     }
 
-    
     getRandomColor() {
         const letters = "0123456789ABCDEF";
-        let color = "#";
-        for (let i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
+        let color = "";
+        for (let i = 0; i < 3; i++) {
+            const component = Math.floor(Math.random() * 256);
+            color += component + ",";
         }
-        return color;
+        return color.slice(0, -1); // Remove the trailing comma
     }
-
 }
-       
-
-     
-
 
 const arr = [];
 for (let i = 0; i < 10; i++) {
@@ -58,11 +62,15 @@ for (let i = 0; i < 10; i++) {
     arr.push(obj);
 }
 
-setInterval( () => {
-    
-        
+function animate() {
+    c.clearRect(0, 0, canvas.width, canvas.height);
 
-for (let i = 0; i < 10; i++) {
-    arr[i].update();
+    for (let i = 0; i < 10; i++) {
+        arr[i].update();
+        arr[i].draw();
+    }
+
+    requestAnimationFrame(animate);
 }
-},200)
+
+animate();
